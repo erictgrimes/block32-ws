6e627d2e3ceabea5e305d005a686ed6e50355241
Eric Grimes <erictgrimes@gmail.com>
2025-07-22 22:23:10 -0700

    removed notes, finalized project

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ export default app;
 
 import employeesRouter from "./api/employees.js";
 
-//preprocessing middleware
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -21,29 +20,4 @@ app.use((err, req, res, next) => {
 
 app.route("/").get((req, res) => {
   res.send("Hello employees!");
-});
-
-// app.route("/employees").get((req, res) => {
-//   res.send(employees);
-// });
-
-// Note: this middleware has to come first! Otherwise, Express will treat
-// "random" as the argument to the `id` parameter of /employees/:id.
-// app.route("/employees/random").get((req, res) => {
-//   const randomIndex = Math.floor(Math.random() * employees.length);
-//   res.send(employees[randomIndex]);
-// });
-
-app.route("/employees/:id").get((req, res) => {
-  const { id } = req.params;
-
-  // req.params are always strings, so we need to convert `id` into a number
-  // before we can use it to find the employee
-  const employee = employees.find((e) => e.id === +id);
-
-  if (!employee) {
-    return res.status(404).send("Employee not found");
-  }
-
-  res.send(employee);
 });
